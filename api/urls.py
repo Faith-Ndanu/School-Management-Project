82119cc9211e82607b4b977515fc5f33e828949e
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-07-17 15:15:32 +0300

    add changes

--- api/urls.py
+++ api/urls.py
@@ -1,29 +1,19 @@
 from django.urls import path
 from .views import StudentListView
-from .views import Course
-from .views import Class
-from .views import Teacher
+from .views import CourseListView
+from .views import ClassListView
+from .views import TeacherListView
 from .views import ClassPeriodListView
 
 
 urlpatterns=[
-    path("classperiods/",ClassPeriodListView.as_view(),name="classperiods_list_view")
+    path("classperiods/",ClassPeriodListView.as_view(),name="classperiods_list_view"),
+    path("teacher/",TeacherListView.as_view(),name="teacher_list_view"),
+    path("students/",StudentListView.as_view(),name="student_list_view"),
+    path("classes/",ClassListView.as_view(),name="classes_list_view"),
+    path("course/",CourseListView.as_view(),name="course_list_view")
+
 ]
 
 
-urlpatterns=[
-    path("teacher/",Teacher.as_view(),name="teacher_list_view")
-]
-
-urlpatterns=[
-    path("students/",StudentListView.as_view(),name="student_list_view")
-]
-
-urlpatterns=[
-    path("course/",Course.as_view(),name="course_list_view")
-]
-
-urlpatterns=[
-    path("classes/",Class.as_view(),name="classes_list_view")
-]
 
